d27c603172eb51a4f803b4435560109348eb58a1
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2023-10-25 13:23:27 +0200

    Add files via upload

---
+++ FM4Z3B_1025/1.feladat/SaxFM4Z3B/src/hu/saxfm4z3b/SaxFM4Z3B.java
@@ -0,0 +1,30 @@
+package hu.saxfm4z3b;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SaxFM4Z3B {
+
+	public static void main(String[] args) {
+		
+		try {
+			XMLReader parser = new SAXParser();
+			
+
+		}
+		catch (IOException ex) {
+			
+		}
+
+	}
+
+}

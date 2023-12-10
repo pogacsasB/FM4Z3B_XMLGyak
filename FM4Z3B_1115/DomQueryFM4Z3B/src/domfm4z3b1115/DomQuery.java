6adff1c1f2105f7e36a4e97208f580c19e86c746
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2023-12-10 22:17:19 +0100

    Update DomQuery.java

--- FM4Z3B_1115/DomQueryFM4Z3B/src/domfm4z3b1115/DomQuery.java
+++ FM4Z3B_1115/DomQueryFM4Z3B/src/domfm4z3b1115/DomQuery.java
@@ -1,43 +1,39 @@
 package domfm4z3b1115;
 
-import java.io.File;
-import java.io.IOException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
+
+import java.io.File;
 
 public class DomQuery {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("kurzusfelvetelFM4Z3B.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(inputFile);
 
-	public static void main(String[] args) {
-		File xml = new File("FM4Z3B_kurzusfelvetel.xml");
-		
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		dbf.setValidating(true);
-		
-		try {
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(xml);
-			
-			NodeList vlista = doc.getElementsByTagName("kurzusnev");
-			
-			for (int p=0; p<vlista.getLength();p++) {
-				 NodeList vgye = vlista.item(p).getChildNodes();
-				 //System.out.println(vgye);
-			}
-		} catch (ParserConfigurationException pce) {
-			System.out.println(pce.getMessage());
-		} catch (SAXException sxe) {
-			System.out.println(sxe.getMessage());
-		} catch (IOException ioe) {
-			System.out.println(ioe.getMessage());
-		}
-		
+            document.getDocumentElement().normalize();
 
-	}
+            System.out.println("Kurzusnevek:");
+            NodeList kurzusnevList = document.getElementsByTagName("kurzusnev");
+            for (int i = 0; i < kurzusnevList.getLength(); i++) {
+                Node node = kurzusnevList.item(i);
+                System.out.println(node.getTextContent());
+            }
 
+            System.out.println("\nOktatok:");
+            NodeList oktatoList = document.getElementsByTagName("oktato");
+            for (int i = 0; i < oktatoList.getLength(); i++) {
+                Node node = oktatoList.item(i);
+                System.out.println(node.getTextContent());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

8efb2aa2912af5896f22102b1a188de205629b08
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2023-12-10 22:20:42 +0100

    Update DomModify.java

--- FM4Z3B_1115/DomQueryFM4Z3B/src/domfm4z3b1115/DomModify.java
+++ FM4Z3B_1115/DomQueryFM4Z3B/src/domfm4z3b1115/DomModify.java
@@ -1,1 +1,42 @@
+package domfm4z3b1115
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+
+import java.io.File;
+
+public class DomModify {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("kurzusfelvetelFM4Z3B.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(inputFile);
+
+            document.getDocumentElement().normalize();
+
+            String targetElementName = "kurzusnev";
+            String newValue = "New Course Name";
+
+            NodeList nodeList = document.getElementsByTagName(targetElementName);
+            if (nodeList.getLength() > 0) {
+                Node node = nodeList.item(0);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    element.setTextContent(newValue);
+                    System.out.println("Element '" + targetElementName + "' modified successfully.");
+                }
+            } else {
+                System.out.println("Element '" + targetElementName + "' not found.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

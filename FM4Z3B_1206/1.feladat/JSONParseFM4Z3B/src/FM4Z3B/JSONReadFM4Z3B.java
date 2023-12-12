9577565b9db8b396334827f2e5859ce729103811
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2023-12-12 23:11:26 +0100

    Update JSONReadFM4Z3B.java

--- FM4Z3B_1206/1.feladat/JSONParseFM4Z3B/src/FM4Z3B/JSONReadFM4Z3B.java
+++ FM4Z3B_1206/1.feladat/JSONParseFM4Z3B/src/FM4Z3B/JSONReadFM4Z3B.java
@@ -1,21 +1,48 @@
 package FM4Z3B;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class JSONReadFM4Z3B {
 
-	public static void main(String[] args) throws IOException, ParseException {
-		JSONParser jsonParser = new JSONParser();
+	public static void main(String[] args) {
 
-		FileReader toParse = new FileReader("JSONFM4Z3B.json");
-		
-		 Object obj = jsonParser.parse(toParse);
-		 
-         System.out.println(obj);
+		JSONParser parser = new JSONParser();
+
+		try {
+			
+			Object obj = parser.parse(new FileReader("orarendFM4Z3B.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+			JSONObject o = (JSONObject) jsonObject.get("FM4Z3B_orarend");
+
+			JSONArray kurzusok = (JSONArray) o.get("kurzusok");
+			for (int i = 0; i < kurzusok.size(); i++) {
+
+				System.out.println(i+1 + ". kurzus:\n");
+				
+				JSONObject a = (JSONObject) kurzusok.get(i);
+				System.out.println("Kurzusnév: " + a.get("kurzusnev"));
+				System.out.println("Kredit: " + a.get("kredit"));
+				System.out.println("Hely: " + a.get("hely"));
+				System.out.println("Időpont: " + a.get("idopont"));
+				System.out.println("Oktató: " + a.get("oktato"));
+				
+				System.out.print("\n");
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
-
 }

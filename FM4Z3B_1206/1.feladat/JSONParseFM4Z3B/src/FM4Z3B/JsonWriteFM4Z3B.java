0edbcd6b1229be76ddd6db5d51b7a8fa35aa3b29
pogacsasB <91243056+pogacsasB@users.noreply.github.com>
2023-12-12 23:13:45 +0100

    Update JsonWriteFM4Z3B.java

--- FM4Z3B_1206/1.feladat/JSONParseFM4Z3B/src/FM4Z3B/JsonWriteFM4Z3B.java
+++ FM4Z3B_1206/1.feladat/JSONParseFM4Z3B/src/FM4Z3B/JsonWriteFM4Z3B.java
@@ -1,29 +1,63 @@
 package FM4Z3B;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+
 public class JsonWriteFM4Z3B {
 
-	public static void main(String[] args) throws IOException {
-		JSONObject employeeDetails = new JSONObject();
-		
-        employeeDetails.put("nev", "Pogacsas Benedek");
-        employeeDetails.put("neptunkod", "FM4Z3B");
-        employeeDetails.put("szak", "GEIK");
-        
-        JSONArray employeeList = new JSONArray();
-        employeeList.add(employeeDetails);
-        
-        System.out.println(employeeList);
-        
-        FileWriter file = new FileWriter("JSONfm4z3b_out.json");
-        
-        file.write(employeeList.toJSONString()); 
-        file.flush();
-	}
+    public static void main(String[] args) {
 
+        try (Reader reader = new FileReader("orarendFM4Z3B.json")) {
+
+            JSONObject JSONObject = new JSONObject();
+
+            JSONArray orak = new JSONArray();
+            orak.add(newOra(new String[]{"Adatkezelés XML-ben", "5", "A1 XXXII magasfsz.","Kedd 12","Dr. Bednarik László"}));
+            orak.add(newOra(new String[]{"Web technológiák 1", "5", "A1 XXX. 3. em.","Hétfő 14","Agárdi Anita"}));
+            orak.add(newOra(new String[]{"WEB alkalmazások(Java)", "5", "In I. em. 102","Kedd 14","Semleci Viktor"}));
+            orak.add(newOra(new String[]{"Fizika II.", "4", "A1 XXX. 3. em.","Hétfő 8","Pszota Gábor"}));
+            orak.add(newOra(new String[]{"Szoftvertesztelés", "5", "In I. em. 103","Hétfő 10","Dr. Hornyák Olivér"}));
+            orak.add(newOra(new String[]{"Mesterséges intelligencia", "5", "A1 XXXII magasfsz.","Kedd 10","Kunné Dr Tamás Judit"}));
+            
+            for (int i = 0; i < orak.size() ;i++) {
+                JSONObject a = (JSONObject) orak.get(i);
+                System.out.println("\n"+ (i + 1) + ". Óra:");
+                System.out.println("Kurzusnév: " + a.get("kurzusnev"));
+				System.out.println("Kredit: " + a.get("kredit"));
+				System.out.println("Hely: " + a.get("hely"));
+				System.out.println("Időpont: " + a.get("idopont"));
+				System.out.println("Oktató: " + a.get("oktato"));
+            }
+
+            JSONObject ora = new JSONObject();
+            ora.put("ora", orak);
+            JSONObject.put("FM4Z3B_orarend", ora);
+
+            FileWriter file = new FileWriter("orarendFM4Z3B1.json");
+            file.write(JSONObject.toString());
+            file.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static JSONObject newOra(String[] s){
+
+    	//kurzusnev, kredit, hely, idopont, oktato
+        JSONObject a = new JSONObject();
+
+        a.put("kurzusnev", s[0]);
+        a.put("kredit", s[1]);
+        a.put("hely", s[2]);
+        a.put("idopont", s[3]);
+        a.put("oktato", s[4]);
+
+        return a;
+    }
 }
